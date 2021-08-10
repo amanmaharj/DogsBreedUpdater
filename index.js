2240476766eb6b82b76c81e97630e088ff5ad0f9
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-08-09 18:31:40 -0600

    display the json file content to the frontend

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const ejsLayout = require('express-ejs-layouts')
+//fs is a built in module
+const fs = require('fs')
 const app = express()
 app.set('view engine', 'ejs')
 
 app.use(ejsLayout)
 app.get('/',(req, res)=>{
-    res.render('home', {name: 'Aman Maharjan', age: 21})
+    const dogs = fs.readFileSync('./dogs.json')
+    //since the data is in json format we need to parse it to display into readable format.
+    const dogData = JSON.parse(dogs)
+    res.render('home', {dogData})
 })
 
 app.listen('4000')
