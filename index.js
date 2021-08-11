e571350dfd65a76edf07605f8b7e3605a0a75eac
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-08-10 19:07:04 -0600

    added a search functionality where user can search by the name of the dog from the list. did work on some front end

--- index.js
+++ index.js
@@ -8,18 +8,34 @@ app.set('view engine', 'ejs')
 app.use(ejsLayout)
 //Whenever we are sending the some kind of data to the url link, it need to be parse i.e. body.parse this middleware help us to do that in more significant way.
 app.use(express.urlencoded({extended: true}))
-//it is a homepage
+
+//It search the dogs in the list, this has to be in top than home route.
+app.get('/dogs', (req, res)=>{
+    let filterData = req.query.nameFilter
+    const dogs = fs.readFileSync('./dogs.json')
+    let dogData = JSON.parse(dogs)
+    
+
+    if(filterData){
+        dogData = dogData.filter((dogDa)=>{
+           return filterData.toLowerCase() === dogDa.name.toLowerCase()
+        })
+        
+    }
+    res.render('home',{myDog: dogData})
+})
+//It is a homepage
 app.get('/dogs',(req, res)=>{
     const dogs = fs.readFileSync('./dogs.json')
     //since the data is in json format we need to parse it to display into readable format.
     const dogData = JSON.parse(dogs)
-    res.render('home', {dogData})
+    res.render('home', {myDog : dogData})
 })
 //It renders the form for creating new name and type of the dogs.
 app.get('/dogs/new',(req,res)=>{
     res.render('new')
 })
-
+//It creates the new data.
 app.post('/dogs/new', (req, res)=>{
     const formName = req.body.name
     const formType = req.body.type
@@ -33,8 +49,9 @@ app.post('/dogs/new', (req, res)=>{
     //saving the form data into the file with the destination name.
     fs.writeFileSync('./dogs.json', JSON.stringify(dogData) )
 
-    res.render('home', {dogData} )
+    res.redirect("/dogs" )
 })
+
 //It gives the specific name and type of the selective dogs.
 app.get('/dogs/:idx', (req,res)=>{
     const dogs = fs.readFileSync('./dogs.json')
@@ -44,6 +61,7 @@ app.get('/dogs/:idx', (req,res)=>{
     const dogIndex = parseInt(req.params.idx)
     res.render('show', {dogsDa : [dogData[dogIndex]]} )
 })
+
 
 
 
