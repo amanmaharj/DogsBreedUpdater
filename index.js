92d1f832daa06b8634f14ef8b55a4b388c0fa1f1
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-08-15 16:54:59 -0600

    the features of editing was built

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const ejsLayout = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 //fs is a built in module
 const fs = require('fs')
 const app = express()
 app.set('view engine', 'ejs')
-
+//middleware
 app.use(ejsLayout)
+app.use(methodOverride('_method'))
 //Whenever we are sending the some kind of data to the url link, it need to be parse i.e. body.parse this middleware help us to do that in more significant way.
 app.use(express.urlencoded({extended: true}))
 
@@ -62,7 +64,42 @@ app.get('/dogs/:idx', (req,res)=>{
     res.render('show', {dogsDa : [dogData[dogIndex]]} )
 })
 
+app.delete('/dogs/:idx',(req, res)=>{
+    const dogs = fs.readFileSync('./dogs.json')
+    let dogData = JSON.parse(dogs)
 
+    //delete the required element with the splice method.
+    dogData.splice(req.params.idx, 1)
 
+    //write the updated list in the json fromat.
+    fs.writeFileSync('./dogs.json', JSON.stringify(dogData))
+    
+    //now redirecting it into the home page after the comletion of the delete function
+    res.redirect('/dogs')
+
+})
+//This is the route to show the edit form part, when the click the edit button it send the index of that dog to be edited
+app.get('/dogs/edit/:idx',(req, res)=>{
+    const dogs = fs.readFileSync('./dogs.json')
+    const dogData = JSON.parse(dogs)
+
+    //we need to send the data of the specific dog by 'dogData[req.params.idx]'
+    res.render('edit.ejs', {myDog: dogData[req.params.idx], dogIndex: req.params.idx })
+})
+
+//This is the route to actually edit the content i.e. name and type
+app.put('/dogs/:idx',(req,res)=>{
+    let dogs = fs.readFileSync('./dogs.json')
+    dogs = JSON.parse(dogs)
+    //it helps to save the name coming from the form into the dogs i.e. json.file
+    dogs[req.params.idx].name = req.body.name 
+    dogs[req.params.idx].type = req.body.type 
+
+    //saving the data
+    fs.writeFileSync('./dogs.json', JSON.stringify(dogs))
+    //redirect into the home page
+    res.redirect('/dogs')
+
+})
 
 app.listen('4000')
